328fbabd5fef854321140395a5a90a1631f046a8
deathcure <101105420+deathcuree@users.noreply.github.com>
2025-07-18 14:50:12 +0800

    Delete server/dist directory
    Delete server/dist directory

--- server/dist/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { createUser } from '../controllers/user.controller.js';
-import { auth, isAdmin } from '../middleware/auth.js';
-const router = express.Router();
-// Create new user (admin only)
-router.post('/', auth, isAdmin, createUser);
-export default router;
