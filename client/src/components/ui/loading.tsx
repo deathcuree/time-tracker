6983dc7216335514e0736369aee597881552ecfd
deathcuree <nikaandaya21@gmail.com>
2025-08-19 14:17:26 +0800

    Update Admin Sidebar.

--- client/src/components/ui/loading.tsx
+++ client/src/components/ui/loading.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const LoadingScreen = () => {
   const [isVisible, setIsVisible] = useState(false);
